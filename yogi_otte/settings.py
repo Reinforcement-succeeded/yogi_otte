e83ff3b4f17256359be432593daef1892c9122f6
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-06-06 12:45:14 +0900

    ADD MySQL DEL sqlite3

--- yogi_otte/settings.py
+++ yogi_otte/settings.py
@@ -77,12 +77,17 @@ WSGI_APPLICATION = 'yogi_otte.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# MySQL Database
+import custom_settings
+
+DATABASES = custom_settings.DATABASES
 
 
 # Password validation
