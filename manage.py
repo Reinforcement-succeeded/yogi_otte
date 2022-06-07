0905d91c34910ebcc8a52a09959fa5e9c7388e48
Minjae <rjsekaehdhkw@gmail.com>
2022-06-07 11:48:43 +0900

    MOD 11:47 수정

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yogiotte.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yogi_otte.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
