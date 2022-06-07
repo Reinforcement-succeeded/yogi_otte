53cd57a0718e4dd235f4e52ae12437489b083cab
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-06-07 12:26:12 +0900

    MOD settings.py
    예쁘게 바꿔놓은? 정도

--- yogi_otte/settings.py
+++ yogi_otte/settings.py
@@ -30,9 +30,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'result',
-    'main',
-    'user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +39,8 @@ INSTALLED_APPS = [
     'store',
     'user',
     'review',
+    'result',
+    'main',
 ]
 
 MIDDLEWARE = [
