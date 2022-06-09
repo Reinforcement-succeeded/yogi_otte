be03029ee71e7ca0fb16533ebdd02ab5b90b5c3b
soiyo <soiyosauce@gmail.com>
2022-06-09 19:52:30 +0900

    RE 전 파일과 같은 파일 다시 업로드

--- result/urls.py
+++ result/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('result/', views.result_view, name='result'),
+    path("result/", views.result_view, name="result"),
 ]
