c271cbebbe6828f270ec146476b59bb4ddc9b03c
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-06-09 10:19:03 +0900

    MOD user/urls.py
    ADD path logout

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('sign-up/', views.sign_up_view, name='sign-up'),
     path('sign-in/', views.sign_in_view, name='sign-in'),
+    path('logout/', views.logout),
 ]
