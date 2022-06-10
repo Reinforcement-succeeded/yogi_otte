a5644364c7457fdd8755d2a015b7a50e08d0a1e9
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-06-10 13:55:26 +0900

    MOD store/views.py
    #some code

--- store/views.py
+++ store/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-import csv
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from .models import Category, Store
-from review import models as R
-from user import models as U
-from datetime import datetime
-from django.utils import timezone
+# import csv
+# import sys, os
+# sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+# from .models import Category, Store
+# from review import models as R
+# from user import models as U
+# from datetime import datetime
+# from django.utils import timezone
 
 # Create your views here.
 def main_view(request):
