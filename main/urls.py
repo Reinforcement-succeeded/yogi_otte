0a8794c55ab2f6cf497bcbf188e62c1c8a9954b8
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-06-10 11:32:15 +0900

    MOD urls.py
    Now on we won't use main app

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("", views.main_view, name="main"),
-]
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#     path('', views.main_view, name='main'),
+# ]
