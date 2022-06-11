d54490838f9cc85f2e5d433849989576cf46338e
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-06-11 10:32:22 +0900

    MOD store/urls.py
    change path

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main_view, name='main'),
-    path("result/<str:category>", views.result_view, name="result"),
+    path("result/<str:category>", views.category_result_view, name="result"),
 ]
