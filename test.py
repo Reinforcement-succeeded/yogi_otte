947a67d54c9f2cc901be7d5c7aaf146fdb031991
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-06-11 23:42:18 +0900

    MOD test.py

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-import csv
-
+# import csv
+# import sys, os
+# sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+# from review import models as R
+# from user import models as U
+# from run_model import sentiment_predict
 #categroy / name
 # cat = []
 # f = open('static/csv/data.csv', 'r', encoding='utf-8')
@@ -9,41 +13,35 @@ import csv
 # for c in set(cat):
 #     Category.objects.create(name = c)
 # f.close()
-#store / name 3, location 6, category 4
+#store / name 3, location 6, category 4, url 2
+#     cat = []
+#     f = open('static/csv/data.csv', 'r', encoding='utf-8')
+#     rdr = csv.reader(f)
+#     for line in rdr:
+#         # print(line)
+#         cat.append((line[3], line[6].split('\n')[0], line[4], line[2]))
+#     print(set(cat))
+#     f.close()
+#     for c in set(cat):
+#         Store.objects.create(name=c[0], location=c[1], category=Category.objects.get(name=c[2]), url = c[3])
+#reviews / store 3, user x, star 5, comment 2, calc_star x
 # cat = []
 # f = open('static/csv/data.csv', 'r', encoding='utf-8')
 # rdr = csv.reader(f)
 # for line in rdr:
-#     # print(line)
-#     cat.append((line[3], line[6].split('\n')[0], line[4]))
-# print(set(cat))
-# f.close()
-# for c in set(cat):
-#     Store.objects.create(name=c[0], location=c[1], category=Category.objects.get(name=c[2]))
-#reviews / store 3, user x, star 5, comment 2
-# cat = []
-# f = open('static/csv/data.csv', 'r', encoding='utf-8')
-# rdr = csv.reader(f)
-# for line in rdr:
-#     cat.append((line[3], line[5], line[1], line[7].replace('.', '-')[:-1] + ' 00:00:00'))
+#     calc = sentiment_predict(line[2])
+#     cat.append((line[3], line[5], line[1], line[7].replace('.', '-')[:-1] + ' 00:00:00', calc * 100))
 # print(set(cat))
 # f.close()
 # for c in set(cat):
 #     try:
-#         print('c[3]=', end=''), print(c[3])
-#
-#         print('datetime - c[3]=', end=''), print(datetime.strptime(c[3], '%Y-%m-%d %H:%M:%S'))
-#         print('b')
-#     except:
-#         pass
-#
-#     try:
-#         R.Review.objects.create(
+#         Review.objects.create(
 #             store=Store.objects.get(name=c[0]),
-#             user=U.CustomUser.objects.get(username=request.user.username),
+#             user=CustomUser.objects.get(username=request.user.username),
 #             star=c[1],
 #             comment=c[2],
-#             create_date=datetime.strptime(c[3], '%Y-%m-%d %H:%M:%S')
+#             create_date=datetime.strptime(c[3], '%Y-%m-%d %H:%M:%S'),
+#             calc_star=c[4]
 #         )
 #     except:
 #         pass
